fecc87e2b1b45640f2b381665948f8a209881e9d
Daniel Juarez <djuarez@car14s6.42barcelona.com>
2025-06-09 19:39:35 +0200

    updated 05/09/2025

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: djuarez <djuarez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/21 18:37:22 by djuarez           #+#    #+#             */
-/*   Updated: 2025/06/09 19:37:14 by djuarez          ###   ########.fr       */
+/*   Updated: 2025/06/09 19:39:27 by djuarez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,8 +46,6 @@ void print_cmds(t_cmd *cmds)
 	while (cmds)
 	{
 		printf("Command:\n");
-
-		// Imprimir argumentos
 		if (cmds->argv)
 		{
 			printf("  argv: ");
@@ -57,21 +55,16 @@ void print_cmds(t_cmd *cmds)
 		}
 		else
 			printf("  argv: (null)\n");
-
-		// Imprimir redirecciones
 		t_redir *r = cmds->redirs;
 		while (r)
 		{
 			printf("  redir type: %d, file: %s\n", r->type, r->file);
 			r = r->next;
 		}
-
-		// Imprimir si hay pipe
 		if (cmds->pipe)
 			printf("  pipe: yes\n");
 		else
 			printf("  pipe: no\n");
-
 		cmds = cmds->next;
 	}
 }
@@ -79,30 +72,22 @@ void print_cmds(t_cmd *cmds)
 int main(void)
 {
 	char *input = "echo hola > out.txt | cat < in.txt";
-
-	// 1. Lexing
 	t_token *tokens = lexer(input);
 	if (!tokens)
 	{
 		printf("Lexer failed.\n");
 		return (1);
 	}
-
-	// 2. Parsing
 	t_cmd *cmds = parser_tokens(tokens);
 	if (!cmds)
 	{
 		printf("Parser failed.\n");
-		free_tokens(tokens); // Si tienes esta función
+		free_tokens(tokens);
 		return (1);
 	}
-
-	// 3. Imprimir resultado
 	print_cmds(cmds);
-
-	// 4. Liberar memoria
-	free_tokens(tokens); // si tienes
-	free_cmds(cmds);     // si tienes
+	free_tokens(tokens);
+	free_cmds(cmds);
 
 	return (0);
 }
